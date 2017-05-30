3381b613bd5769b2f2814c06c8709a0c421ee30b
Todor Chenkov <joto_to@mail.bg>
2017-05-30 23:29:28 +0300

    push upper half

--- DenisTodorGitTest/DenisTodorGitTest/DenisTodorGitTest.cs
+++ DenisTodorGitTest/DenisTodorGitTest/DenisTodorGitTest.cs
@@ -8,8 +8,21 @@ namespace DenisTodorGitTest
         {
             // upper half
 
-
-
+            int dressSize = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}{1}{0}", new string('.', dressSize), new string('*', dressSize));
+            int sides = dressSize;
+            for (int i = 0; i < dressSize / 2; i++)
+            {
+                sides -= 2;
+                Console.WriteLine("{0}*{1}*{0}", new string('.', sides), new string('.', dressSize*3 - (sides*2 + 2)));
+            }
+            Console.WriteLine("*{0}*{1}*{0}*", new string('.', dressSize - 2), new string('.', dressSize));
+            sides = 1;
+            for (int i = 0; i < (dressSize / 2)-1; i++)
+            {
+                Console.WriteLine("*{0}*{1}*{2}*{1}*{0}*", new string('.', dressSize - 3 - sides), new string('.', sides), new string('.', dressSize));
+                sides += 2;
+            }
             // lower half
 
 
