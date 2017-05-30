4fd9474d18db12f6fadb89749d6af7b253d47a42
ReapeR-MaxPayne <reaper.madmaxpain@gmail.com>
2017-05-30 21:26:31 +0200

    Added team solution.

---
+++ ProblemForTestRepo/Problem_Hornet Comm/Program_Hornet_Comm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Problem_Hornet_Comm
+{
+    class Program_Hornet_Comm
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
