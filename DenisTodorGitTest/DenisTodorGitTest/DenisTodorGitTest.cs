31b47881066186ee19b80a1749d4d52cc9f8b82f
DenisPetrov92 <gardenis@abv.bg>
2017-05-30 23:25:05 +0300

    Done the lower half of DenisTodorGitTest.cs

--- DenisTodorGitTest/DenisTodorGitTest/DenisTodorGitTest.cs
+++ DenisTodorGitTest/DenisTodorGitTest/DenisTodorGitTest.cs
@@ -11,7 +11,15 @@ namespace DenisTodorGitTest
 
 
             // lower half
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < n - 1; i++)
+            {
+                Console.WriteLine("{0}*{1}*{0}",
+                    new string('.', n - 1 - i),
+                    new string('.', n + 2 * i));
+            }
+            Console.WriteLine("{0}", new string('*', 3 * n));
 
         }
     }
